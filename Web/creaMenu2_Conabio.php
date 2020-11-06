2355379170781ed8eb5dd2669499134512ba8e91
RanyartRodrigo <ranyart.rodrigo@gmail.com>
2020-11-05 22:46:50 -0600

    -Añadir header en Web/crea_menu2Conabio.php para cargar acentos

--- Web/creaMenu2_Conabio.php
+++ Web/creaMenu2_Conabio.php
@@ -2,6 +2,7 @@
 	//ini_set('display_errors', 1);
 	//ini_set('display_startup_errors', 1);
 	//error_reporting(E_ALL);
+header('Content-Type: text/html; charset=UTF-8');
 	include "../Panel/base.php";
 	include "funciones.php";
 	include "../Panel/host2.php";
