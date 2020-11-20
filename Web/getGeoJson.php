1ebab286bec276cda4968fc8a46bd0606d44201a
Roberto Rangel <mcrangelheras@gmail.com>
2020-11-20 16:43:47 -0500

    Optimizado

--- Web/getGeoJson.php
+++ Web/getGeoJson.php
@@ -1,11 +1,12 @@
 <?php
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
+	ini_set('memory_limit', '-1');
 	error_reporting(E_ALL);
-
+	header('Content-Encoding: gzip');
 	$file = $_REQUEST['file'];
 	$result = file_get_contents($file);
 	//$array = json_decode($result, true);
 	//echo json_encode($array);
-	echo json_encode($result);
+	echo gzencode(json_encode($result));
 ?>
